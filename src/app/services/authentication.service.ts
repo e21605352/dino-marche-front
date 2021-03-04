5bfbf8704726f5f93c93e6799792e20b6ce1f145
Théo Yvonnou <tyvonnou@gmail.com>
2021-03-04 22:47:47 +0100

    Ajout headers requetes

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -1,20 +1,28 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { baseUrl } from 'src/environments/environment';
+import { TokenManager } from '../authentication/tokenManager/TokenManager';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
 
-  constructor(private http: HttpClient) { }
+  private header: any;
+
+  constructor(private http: HttpClient, private tokenManager: TokenManager) {
+    this.header = {
+      headers: new HttpHeaders()
+        .set('Authorization', `Basic ${btoa(tokenManager.retrieve())}`)
+    }
+  }
 
   login(data: any): Observable<any> {
-    return this.http.post(`${baseUrl}customer`, data);
+    return this.http.post(`${baseUrl}customer`, data, this.header);
   }
 
   createAccount(data: any): Observable<any> {
-    return this.http.post(`${baseUrl}customer/create`, data);
+    return this.http.post(`${baseUrl}customer/create`, data, this.header);
   }
 }
