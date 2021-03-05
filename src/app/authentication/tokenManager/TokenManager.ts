8d65cda9c2d3d1a506cf19805ebd23285aaa6e12
Théo Yvonnou <tyvonnou@gmail.com>
2021-03-05 10:35:12 +0100

    Ajout filtre sur connexion

--- src/app/authentication/tokenManager/TokenManager.ts
+++ src/app/authentication/tokenManager/TokenManager.ts
@@ -7,15 +7,15 @@ export class TokenManager {
     private tokenKey: string = 'app_token';
 
     public store(content: string) {
-        localStorage.setItem(this.tokenKey, content);
+        sessionStorage.setItem(this.tokenKey, content);
     }
 
     public retrieve() {
-        return localStorage.getItem(this.tokenKey);
+        return sessionStorage.getItem(this.tokenKey);
     }
 
     public getDecodedAccessToken(): any {
-        let res = localStorage.getItem(this.tokenKey);
+        let res = sessionStorage.getItem(this.tokenKey);
         if (res != null) return jwt_decode(res);
         return null;
     }
